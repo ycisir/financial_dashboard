e1fb3856f64aa3982a08256073d0e670e521692e
yasir <mohammadyasirkmt@gmail.com>
2026-04-03 23:48:31 +0530

    update api endpoints on index and add seed command

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,23 +4,21 @@ class HomeController < ApplicationController
       message: "Finance Dashboard System Endpoints",
       endpoints: {
         users: {
-          list: "http://127.0.0.1:3000/users",
-          create: "http://127.0.0.1:3000/users [POST]",
-          update: "http://127.0.0.1:3000/users/:id [PATCH]",
-          change_status: "http://127.0.0.1:3000/users/:id/status [PATCH]"
+          list: "/users",
+          create: "/users [POST]",
+          update: "/users/:id [PATCH]",
+          change_status: "/users/:id/status [PATCH]"
         },
         records: {
-          list: "http://127.0.0.1:3000/financial_records",
-          list_filter_by_record_type: "http://127.0.0.1:3000/financial_records?record_type=income",
-          list_filter_by_category: "http://127.0.0.1:3000/financial_records?category=bills",
-          list_filter_by_date: "http://127.0.0.1:3000/financial_records?start_date=2026-03-21&end_date=2026-03-31",
-          create: "http://127.0.0.1:3000/financial_records [POST]",
-          update: "http://127.0.0.1:3000/financial_records/:id [PATCH]",
-          delete: "http://127.0.0.1:3000/financial_records/:id [DELETE]",
+          list: "/financial_records",
+          list_filter_by_record_type: "/financial_records?record_type=income",
+          list_filter_by_category: "/financial_records?category=bills",
+          list_filter_by_date: "/financial_records?start_date=2026-03-21&end_date=2026-03-31",
+          create: "/financial_records [POST]",
+          update: "/financial_records/:id [PATCH]",
+          delete: "/financial_records/:id [DELETE]",
         },
-        dashboard: {
-          summary: "http://127.0.0.1:3000/financial_records/summary"
-        }
+        dashboard_summary: "/financial_records/summary"
       }
     }
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+user = User.create(name: "Test User", email: "test@example.com", role: "admin", status: "active")
